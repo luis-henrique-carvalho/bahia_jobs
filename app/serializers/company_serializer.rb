99f4da87c0503c97a4423d2cb339b67cc72f1764
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-05-30 23:04:34 -0300

    chore: Update routes and models for applicants and positions

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  url        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :uuid             not null
+#
+# Indexes
+#
+#  index_companies_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class CompanySerializer < ActiveModel::Serializer
   attributes :id, :name, :url
   has_one :user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :companies
-  resources :positions
   resources :applicants
+  resources :positions
+  resources :companies
   root 'home#index'
   devise_for :users
 
