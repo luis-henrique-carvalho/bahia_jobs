1750bd8bf3c7355b01525ebf4072a6b8dd097c57
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-06-08 01:48:00 -0300

    feat: Add additional fields and validations to company form

--- spec/factories/company.rb
+++ spec/factories/company.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory(:company) do
-    name { Faker::Company }
+    name { Faker::Company.name }
+    # create a big text for summary
+    summary { Faker::Lorem.paragraph_by_chars(number: 1000, supplemental: false) }
+    founded_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    employee_count { Faker::Number.between(from: 1, to: 1000) }
+    contact_email { Faker::Internet.email }
+    contact_phone { Faker::PhoneNumber.cell_phone }
     url { Faker::Internet.url }
 
     association :user
